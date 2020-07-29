946f53b2ce78d6814341a79bd1a1c0bd50bd07bb
vobecant <47218173+vobecant@users.noreply.github.com>
2020-07-29 16:41:23 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,38 +1,30 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Antonin Vobecky | PhD student in Computer Vision',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'Antonin Vobecky is a PhD student at the CTU in Prague under the supervision of Josef Sivic who specializes in Computer Vision.',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
-  siteUrl: 'https://brittanychiang.com',
+    'Antonin Vobecky, Antonin, Vobecky, vobecant, PhD student, Computer Vision',
+  siteUrl: 'https://https://vobecant.netlify.app/',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Antonin Vobecky',
+  location: 'Prague, Czech Republic',
+  email: 'a.vobecky@gmail.com',
+  github: 'https://github.com/vobecant',
+  twitterHandle: '@AVobecky',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/vobecant',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.linkedin.com/in/antoninvobecky',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/AVobecky',
     },
   ],
 
